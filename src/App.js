4d0edc28af089b7096fa8e409b1ef6a857d69b4b
vaibhavfirke <firkevaibhav11@gmail.com>
2023-05-02 13:35:53 +0530

    updating id

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import { Box } from "@chakra-ui/react";
 import "./App.css";
-import Home from "./Conponents/Home";
-import Navbar from "./Conponents/Navbar"
-import About from "./Conponents/About"
-import Skills from "./Conponents/Skills";
-import Project from "./Conponents/Project";
-import Git from "./Conponents/git";
-import Contacts from "./Conponents/Contacts"
-import Nav from "./Conponents/nav"
+import Home from "./Components/Home";
+import Navbar from "./Components/Navbar"
+import About from "./Components/About"
+import Skills from "./Components/Skills";
+import Project from "./Components/Project";
+import Git from "./Components/git";
+import Contacts from "./Components/Contacts"
+import Nav from "./Components/nav"
 function App() {
   return (
     <Box  bg="#013045" >
        {/* <Navbar/> */}
        <Nav/>
-      <Box
+      {/* <Box
         position="relative"
         height="auto"
         width="100%"
        
-      >
+      > */}
       <Home />
-      </Box>
+      {/* </Box> */}
       <About/>
       <Skills />
       <Project/>
